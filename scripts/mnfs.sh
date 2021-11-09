91e6fa7deac2731daa441f988c3099cf5bee9cd2
Meekrat34 <dhebard@gmail.com>
2021-11-08 22:59:50 -0600

    minor script update

--- scripts/mnfs.sh
+++ scripts/mnfs.sh
@@ -3,4 +3,4 @@
 # created by Meekrat : Oct2020
 # mounts nfs on my laptop
 
-sudo mount -t nfs 192.168.0.156:/home/dave/share /home/dave/share
+sudo mount -t nfs ip:/home/user/share /home/user/share
